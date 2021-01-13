88dafb45c139e1f4deb8c74d2883a17831f7511f
deepthi-janga <deepthi.janga@zemosolabs.com>
2021-01-13 19:30:53 +0530

    Tell a short story

---
+++ Assignment2.js
@@ -0,0 +1,21 @@
+/*
+I want you to create a short story.
+
+Create variables and use string concatenation to build your story.
+
+Here is the template for the story.
+
+In a <place> not so far away, <personOrAnimal> saw that there was no <thing>.
+
+Example story: "In a farm not so far away, chicken saw that there was no corn."
+
+Questions for this assignment
+Please complete the tasks outlined in the instructions video. Did you have any problems while completing the assignment?
+*/
+
+var place="Hyderabad";
+var personOrAnimal = "Mr. Jackson";
+var thing = "Ships.";
+
+var combinedString = "In a "+place+" not so far away, "+personOrAnimal+" saw that there was no "+thing;
+console.log("Short story is " +combinedString)
