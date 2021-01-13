ce51116ba6e9d8a9b66a0e01d5b08593de1232dc
deepthi-janga <deepthi.janga@zemosolabs.com>
2021-01-13 19:22:06 +0530

    Variable library

---
+++ Assignment1.js
@@ -0,0 +1,31 @@
+/*
+I want you to create 10 variables.
+
+The variable names are going to be book1, book2, book3, etc..
+
+Give each book the correct value based on the book list I have given.
+
+Questions for this assignment
+Please complete the tasks outlined in the instructions video. Did you have any problems while completing the assignment?
+*/
+
+var book1 = "Bee in the Hive"
+var book2 = "The Secret Life of the Ant"
+var book3 = "Laptops. Not Just for Breakfast AnyMore"
+var book4 = "If I were a Superhero"
+var book5 = "Once Upon a Time in the Rabbit Mole"
+var book6 = "T"
+var book7 = "In Memory of the Last Slice of Pizza"
+var book8 = "Who Stole Second Base"
+var book9 = "2010, The Coding Odyssey"
+var book10 = "The End"
+console.log("Book 1 name is " +book1)
+console.log("Book 2 name is " +book2)
+console.log("Book 3 name is " +book3)
+console.log("Book 4 name is " +book4)
+console.log("Book 5 name is " +book5)
+console.log("Book 6 name is " +book6)
+console.log("Book 7 name is " +book7)
+console.log("Book 8 name is " +book8)
+console.log("Book 9 name is " +book9)
+console.log("Book 10 name is " +book10)
