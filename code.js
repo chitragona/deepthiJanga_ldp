b9a4ab0f11417b10d85bce353267ab4f3bc77636
deepthi-janga <41574673+deepthi-janga@users.noreply.github.com>
2021-01-19 11:09:01 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
+/* Create a function that takes an array as a parameter and compares the values of the array.
+Print out the number of matches.
+Here is an example:
+[1, 1, 2, 3]
+1 and 1 are the same.
+1 and 2 are different.
+Print out would have a count of 1 match.
+Write some tests for your function.
+Questions for this assignment
+Please complete the tasks outlined in the instructions video. Did you have any problems while completing the assignment? */
+
 function compare(myArr) {
     for (var i = 0; i < myArr.length; i++) {
       for (var j = i + 1; j < myArr.length; j++) {
