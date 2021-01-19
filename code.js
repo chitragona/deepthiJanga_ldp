ea5449405c3a3521340160d75673ea935f62bde5
deepthi-janga <41574673+deepthi-janga@users.noreply.github.com>
2021-01-19 11:09:54 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
+/* A Palindrome is a word or phrase that is the same forwards of backwards.
+Example: "Anna" backwards is "Anna"
+Write a function that checks if a word or phrase is a palindrome.
+Write some tests for your function.
+Questions for this assignment
+Please complete the tasks outlined in the instructions video. Did you have any problems while completing the assignment?*/
+
 function Pal(word){
 
     let splitword = word.split("")
