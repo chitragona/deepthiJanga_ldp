4199d4d5825ad5febfe26a5fd0e78eee693e0f37
deepthi-janga <41574673+deepthi-janga@users.noreply.github.com>
2021-01-19 10:18:05 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
-function fizzBuzz(start,end) {
+/* Create a function that does the following:
+- The function must take 2 numbers. A start and an end.
+- The function should check each number in the range given to see if it meets one of these criteria:
+    - If the number is divisible by 3 print out "Fizz"
+    - If the number is divisible by 5 print out "Buzz"
+    - If the number is divisible by both 3 and 5, print out "FizzBuzz"
+    - If none of the above is true then just print the number
+Write some tests for your function.
+Questions for this assignment
+Please complete the tasks outlined in the instructions video. Did you have any problems while completing the assignment?*/function fizzBuzz(start,end) {
 
     if (end>start){
      
